f2f8c57550b4996bc9455df31f1def0dbc835da4
Ryuichi Lun <ryuichi.y.lun@gmail.com>
2022-10-07 20:01:20 +0900

    Hello.cpp clean code
    Updated the hello world to be clean code.

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,7 @@
 //hello world implementation for C++
 #include <iostream>
 using namespace std;
-int main(void){
-    cout << "Hello,world";
+int main() {
+    cout << "Hello, World!" << endl;
+    return 0;
 }
